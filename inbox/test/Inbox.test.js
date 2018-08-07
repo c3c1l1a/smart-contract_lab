6501e9c820358fd60b4b4c80c5af5a3abb894da3
c3c1l1a <cesswangumukima@gmail.com>
2018-08-07 16:19:34 +0800

    connected web3 to ganache

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -1,3 +1,15 @@
+
 const assert = require('assert');
 const ganache = require('ganache-cli');
-const Web3 = require('web3'); //this is the constructor and thus is save with a variable starting with caps
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider);
+
+
+/*
+NOTES
+1) Uppercase Web3 - is the call to the contructor function
+   Lowercase web3 - is the web3 instance
+2) Provider - This is the communucation layer between web3 and some specific ethereum network
+3) Ganache is the local ethereum test network.
+
+*/

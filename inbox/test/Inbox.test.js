79c61c2d1c2771776be04560938d74dc363cb44a
c3c1l1a <cesswangumukima@gmail.com>
2018-08-07 16:09:48 +0800

    essential libraries

---
+++ inbox/test/Inbox.test.js
@@ -0,0 +1,3 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3'); //this is the constructor and thus is save with a variable starting with caps

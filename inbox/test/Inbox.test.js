5a78328e3f0a99793ffaf3b5704ad0c6cfa09e0e
c3c1l1a <cesswangumukima@gmail.com>
2018-08-09 13:33:07 +0800

    setup mocha test

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -2,9 +2,22 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3 = new Web3(ganache.provider);
+const web3 = new Web3(ganache.provider());
 
 
+beforeEach (() => {
+  // Get a list of all accounts
+  web3.eth.getAccounts()
+    .then(fetchedAccounts => {
+      console.log(fetchedAccounts);
+    });
+});
+
+describe('Inbox', () => {
+  it('deploys contract', () => {
+
+  });
+});
 /*
 NOTES
 1) Uppercase Web3 - is the call to the contructor function

5862541832c87c0396cd020ba1c5692a203cb4cc
c3c1l1a <cesswangumukima@gmail.com>
2018-08-14 10:05:14 +0800

    test contract deployment and default message modification

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -8,19 +8,32 @@ const {interface, bytecode} = require('../compile.js');
 let accounts;
 let inbox;
 
+const DEFAULT_MESSAGE = "Hi there";
+
 beforeEach (async () => {
   // Get a list of all accounts
   accounts = await web3.eth.getAccounts();
 
   // Use one of the accounts to deploys the contract
   inbox = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({data: bytecode, arguments: ["Hi there!"]})
+    .deploy({data: bytecode, arguments: [DEFAULT_MESSAGE]})
     .send({from: accounts[0], gas: '1000000'});
 });
 
 describe('Inbox', () => {
   it('deploys contract', () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, DEFAULT_MESSAGE);
+  });
+
+  it('can change the message', async () => {
+    await inbox.methods.setMessage("what").send({from: accounts[0]});
+    const message = await inbox.methods.message().call();
+    assert.equal(message, "what");
   });
 });
 /*
